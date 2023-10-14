16d55b8c5c9a254acd6e491d300c14d6fbafc2ed
Berta González <bertagonzalezmartin@gmail.com>
2023-10-14 18:12:50 +0200

    Add initWorld method at world class

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-import Cell from "./Cell/Cell";
+import Cell from "./Cell/Cell.js";
+import World from "./World/World.js";
+
+const cellLength = 3;
+
+const cell = new Cell(false);
+const grid = new World(cell);
+
+grid.initWorld(cellLength);
+
+console.log(grid);
+console.log(cell);
